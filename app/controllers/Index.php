f6384724988ce2fad761e25212373cb8559bf0ff
oblind <oblind@163.com>
2019-05-22 18:13:42 +0800

    IE浏览器处理

--- app/controllers/Index.php
+++ app/controllers/Index.php
@@ -2,6 +2,7 @@
 
 class IndexController extends Yaf\Controller_Abstract {
   function indexAction() {
-    echo file_get_contents(APP_PATH . '/resources/html/index.htm');
+    $f = strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') ? 'ie.htm' : 'index.htm';
+    echo file_get_contents(APP_PATH . "/resources/html/$f");
   }
 }
